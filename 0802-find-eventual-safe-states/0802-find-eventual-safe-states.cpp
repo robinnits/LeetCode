78cac1cd468420bee2fb5108aa640add22bbcd6c
Robin Poddar Partha <robinpoddar2003@gmail.com>
2026-08-29 10:03:00 +0530

    Time: 51 ms (40.54%), Space: 66.2 MB (48.3%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int>eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> revgraph(n);
+        vector<int> indegree(n);
+        for(int i =0; i<n; i++) {
+            for(auto it : graph[i]) {
+                revgraph[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int> q;
+        vector<int> ans;
+        for(int i=0; i<n; i++) {
+            if(indegree[i] == 0) q.push(i);
+        }
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : revgraph[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
